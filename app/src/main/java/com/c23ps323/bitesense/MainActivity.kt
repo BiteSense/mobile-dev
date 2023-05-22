c36836e1675c92ce9785f78643727cd8772a38dc
Rifqy232 <rachmanmry20d@student.unhas.ac.id>
2023-05-22 15:39:14 +0800

    first init

---
+++ app/src/main/java/com/c23ps323/bitesense/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.c23ps323.bitesense
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
